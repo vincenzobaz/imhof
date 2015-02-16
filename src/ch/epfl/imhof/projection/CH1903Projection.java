8ad2514e4fa49424e1f17b513f8f7599a0a814e0
Vincenzo <vincenzo.bazzucchi@epfl.ch>
2015-02-16 18:11:17 +0100

    petite modificatio classe CH1903....

--- src/ch/epfl/imhof/projection/CH1903Projection.java
+++ src/ch/epfl/imhof/projection/CH1903Projection.java
@@ -1,12 +1,23 @@
 package ch.epfl.imhof.projection;
-
+package java.util.Scanner;
+import ch.epfl.imhof.geometry.*;
+import ch.epfl.imhof.PointGeo
 // rendre immuable
 
 public final class CH1903Projection {
-    Point project(PointGeo point){
-        private lambda = ()
+    
+    public Point project(PointGeo point){
+        private double lon = (point.longitude()*3600 - 26782.5)/10000;
+        private double lat = (point.latitude()*3600 - 169028.66 )/10000;
+        return new Point(600072.37
+                + 211455.93*lon
+                - 10938.51*lon*lat
+                - 0.36 * lon * Math.pow(lat,2)
+                - 44.54 * Math.pow(lon,3));
     }
 
-    PointGeo inverse(Point point);
+    public PointGeo inverse(Point point);
 
 }
+longitude
+latitude
