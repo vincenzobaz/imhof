fdb17cb666cda18107ec2b3996a9d266a2dbc1b0
panv <panv@hotmail.fr>
2015-02-17 21:45:46 +0100

    spellcheck

--- src/ch/epfl/imhof/projection/EquirectangularProjection.java
+++ src/ch/epfl/imhof/projection/EquirectangularProjection.java
@@ -4,19 +4,19 @@ import ch.epfl.imhof.PointGeo;
 import ch.epfl.imhof.geometry.Point;
 
 /**
- * coordonnées sphériques d'un point à la surface de la Terre en cordonnées
- * cartésiennes et viceversa dans le cadre de la projection équirectangulaire
+ * Conversion des coordonnées sphériques d'un point à la surface de la Terre en coordonnées
+ * cartésiennes et viceversa dans le cadre de la projection équirectangulaire.
  * Cette classe ne contient que des méthodes. La classe est immuable.
  * 
- * @author Vincenzo Bazzucchi (249733), Nicolas Phan Van (239293) Conversion des
+ * @author Vincenzo Bazzucchi (249733), Nicolas Phan Van (239293)
  * 
  */
 public final class EquirectangularProjection implements Projection {
 
-
     public Point project(PointGeo point) {
         return new Point(point.longitude(), point.latitude());
     }
+    
     public PointGeo inverse(Point point) {
         return new PointGeo(point.x(), point.y());
     }
