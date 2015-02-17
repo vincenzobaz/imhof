ca5806ae076641b491c4869e64e98fa5b3f21382
phanvan <phanvan@localhost>
2015-02-17 16:09:53 +0100

    mon 1er commit!! compléter la classe PointGeo

--- src/ch/epfl/imhof/PointGeo.java
+++ src/ch/epfl/imhof/PointGeo.java
@@ -5,6 +5,17 @@ public final class PointGeo {
     private final double latitude;
     
     public PointGeo(double longitude, double latitude) {
+        try {
+            if (testLongitude(longitude)) {
+                throw new IllegalArgumentException("La Longitude doit être comprise entre -Pi et Pi");
+            }
+            if (testLatitude(latitude)) {
+                throw new IllegalArgumentException("La latitude doit être comprise entre -PI/2 et Pi/2");
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
         this.longitude = longitude;
         this.latitude = latitude;
     }
@@ -15,5 +26,13 @@ public final class PointGeo {
     
     public double latitude() {
         return latitude;
+    }
+    
+    private boolean testLongitude(double longitude) {
+        return (longitude < Math.PI || longitude > Math.PI);
+    }
+    
+    private boolean testLatitude(double latitude) {
+        return (latitude < Math.PI / 2 || latitude > Math.PI / 2));
     }
 }
