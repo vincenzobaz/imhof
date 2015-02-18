fdfb5e2c95126985947ecbddf79adc52083b5160
Vincenzo <vincenzo.bazzucchi@epfl.ch>
2015-02-18 14:17:17 +0100

    Created PolyLine OpenPolyLine Polygon

---
+++ src/ch/epfl/imhof/geometry/PolyLine.java
@@ -0,0 +1,47 @@
+package ch.epfl.imhof.geometry;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Classe abstraite définissant ll'entité polyligne.
+ * On fournit deux getters: l'un pour le premier point et l'autre pour la liste entière.
+ * La classe est immuable
+ * @author Vincenzo Bazzucchi (249733), Nicolas Phan Van (239293)
+ *
+ */
+public abstract class PolyLine {
+    private List<Point> points;
+
+    /**
+     * 
+     * @param points
+     *            La liste des points composant la ligne
+     * 
+     * @throws IllegalArgumentException
+     *             lancée si la liste passé en paramètre est vide
+     */
+    public PolyLine(List<Point> points) throws IllegalArgumentException {
+        try {
+            if (points.size() == 0) {
+                throw new IllegalArgumentException(
+                        "La liste des points est vide");
+            }
+            this.points = Collections.unmodifiableList(points);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
+    }
+
+    public abstract boolean isClosed();
+
+    public List<Point> points() {
+        return points;
+    }
+
+    public Point firstPoint() {
+        return points.get(0);
+    }
+
+}
---
+++ src/ch/epfl/imhof/geometry/Polygon.java
@@ -0,0 +1,25 @@
+package ch.epfl.imhof.geometry;
+import java.util.List;
+import java.util.Collections;
+
+public final class Polygon {
+    private ClosedPolyLine shell;
+    private List<ClosedPolyLine> holes;
+    
+    public Polygon(ClosedPolyLine shell, List<ClosedPolyLine> holes){
+        this.shell = shell;
+        this.holes = Collections.unmodifiableList(holes);
+    }
+    
+    public Polygon(ClosedPolyLine shell){
+        this.shell = shell;
+    }
+    
+    public PolyLine shell(){
+        return shell;
+    }
+    
+    public List<PolyLine> holes() {
+        return holes;
+    }
+}
