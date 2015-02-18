21c2f06282b2300c5573b318bbe6b9a298444b87
Nicolas <panv@hotmail.fr>
2015-02-18 19:37:32 +0100

    test résolution conflit

--- src/ch/epfl/imhof/geometry/ClosedPolyLine.java
+++ src/ch/epfl/imhof/geometry/ClosedPolyLine.java
@@ -6,7 +6,7 @@ public final class ClosedPolyLine extends PolyLine {
     public ClosedPolyLine(List<Point> points) throws IllegalArgumentException {
         super(points);
         try {
-            if (samePoint(this.points().get(0), this.points().get(this.points().size() - 1)) {
+            if (samePoint(this.firstPoint(), this.points().get(this.points().size() - 1))) {
                 throw new IllegalArgumentException ("Le 1er point est différent du dernier");
             }
         }
@@ -28,17 +28,19 @@ public final class ClosedPolyLine extends PolyLine {
         return (point1.x() == point2.x() && point1.y() == point2.y());
     }
     
-    private boolean isOnTheLeft (Point p, Point a, Point b){
-        return  (signedTriangleArea(p, a, b)>0);
+    private boolean isOnTheLeft (Point p, Point a, Point b) {
+        return (signedTriangleArea(p, a, b) > 0);
     }
     
-    private double signedTriangleArea(Point a, Point b, Point c){
-        return 0.5*((b.x()- a.x())*(c.y()-a.y())-(c.x()-a.x())*(b.y()-a.y()));
+    private double signedTriangleArea(Point a, Point b, Point c) {
+        return 0.5 * ((b.x() - a.x()) * (c.y() - a.y()) - (c.x() - a.x()) * (b.y() - a.y()));
     }
     
     private Point getVertex(int n) {
-        return points().get(floorMod(n, points().size()));
+        return points().get(Math.floorMod(n, points().size()));
     }
     
-    public boolean containsPoint(Point p){return false;}
+    public boolean containsPoint(Point p) {
+        return false;
+    }
 }
