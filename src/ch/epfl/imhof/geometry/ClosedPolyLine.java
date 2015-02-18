7e48fefaee51f76b7c58d2708dbb48da1e90e281
Vincenzo <vincenzo.bazzucchi@epfl.ch>
2015-02-18 14:42:22 +0100

    working on ClosedPolyLine

--- src/ch/epfl/imhof/geometry/ClosedPolyLine.java
+++ src/ch/epfl/imhof/geometry/ClosedPolyLine.java
@@ -6,12 +6,12 @@ public final class ClosedPolyLine extends PolyLine {
     public ClosedPolyLine(List<Point> points) throws IllegalArgumentException {
         super(points);
         try {
-            if (samePoint(this.points().get(0), this.points.get(this.points.size() - 1)) {
+            if (samePoint(this.points().get(0), this.points().get(this.points().size() - 1)) {
                 throw new IllegalArgumentException ("Le 1er point est différent du dernier");
             }
         }
         catch (IllegalArgumentException e) {
-            System.out.Println(e.getMessage());
+            System.out.println(e.getMessage());
             throw e;
         }
     }
@@ -28,5 +28,12 @@ public final class ClosedPolyLine extends PolyLine {
         return (point1.x() == point2.x() && point1.y() == point2.y());
     }
     
+    private boolean isOnTheLeft (Point p, Point a, Point b){
+        return  (signedTriangleArea(p, a, b)>0);
+    }
+    
+    private double signedTriangleArea(Point a, Point b, Point c){
+        return 0.5*((b.x()- a.x())*(c.y()-a.y())-(c.x()-a.x())*(b.y()-a.y()));
+    }
     
 }
