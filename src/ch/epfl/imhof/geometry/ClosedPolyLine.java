8b349bfbaf17940f822eb1b4cab962b27f125f19
phanvan <phanvan@localhost>
2015-02-18 14:51:30 +0100

    working on ClosedPolyLine

--- src/ch/epfl/imhof/geometry/ClosedPolyLine.java
+++ src/ch/epfl/imhof/geometry/ClosedPolyLine.java
@@ -6,12 +6,12 @@ public final class ClosedPolyLine extends PolyLine {
     public ClosedPolyLine(List<Point> points) throws IllegalArgumentException {
         super(points);
         try {
-            if (samePoint(this.points().get(0), this.points.get(this.points.size() - 1)) {
+            if (samePoint(this.points().get(0), this.points().get(this.points().size() - 1)) {
                 throw new IllegalArgumentException ("Le 1er point est différent du dernier");
             }
         }
         catch (IllegalArgumentException e) {
-            System.out.Println(e.getMessage());
+            System.out.println(e.getMessage());
             throw e;
         }
     }
@@ -28,5 +28,7 @@ public final class ClosedPolyLine extends PolyLine {
         return (point1.x() == point2.x() && point1.y() == point2.y());
     }
     
-    
+    private Point getVertex(int n) {
+        return points().get(floorMod(n, points().size()));
+    }
 }
