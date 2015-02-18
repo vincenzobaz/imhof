a9993de93d4e63a65791316d906e01c152b3b5fb
Vincenzo <vincenzo.bazzucchi@epfl.ch>
2015-02-18 19:07:27 +0100

    Premier brouillon de - la méthode containsPoint - la méthode area - téléchargé et importé les tests de l'étape 2. 	- on obtient une erreur bizarre avec le constructeur de la classe ClosedPolyLine: il dit que holes reçu en paramètre n'est pas cloné avant d'être affecté mais je l'ai rendu non-modifiable... ça devrait être suffisant. Il nous manque la partie sur les builders

--- src/ch/epfl/imhof/geometry/ClosedPolyLine.java
+++ src/ch/epfl/imhof/geometry/ClosedPolyLine.java
@@ -6,41 +6,61 @@ public final class ClosedPolyLine extends PolyLine {
     public ClosedPolyLine(List<Point> points) throws IllegalArgumentException {
         super(points);
         try {
-            if (samePoint(this.points().get(0), this.points().get(this.points().size() - 1))) {
-                throw new IllegalArgumentException ("Le 1er point est différent du dernier");
+            if (samePoint(this.points().get(0),
+                    this.points().get(this.points().size() - 1))) {
+                throw new IllegalArgumentException(
+                        "Le 1er point est différent du dernier");
             }
-        }
-        catch (IllegalArgumentException e) {
+        } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             throw e;
         }
     }
-    
+
     public boolean isClosed() {
         return true;
     }
-    
-    public double area(){
-        Point origine = new Point(0,0);
+
+    public double area() {
+        Point origine = new Point(0, 0);
+        double area =0;
+        for (int i=0; i<points().size(); i++){
+            area += signedTriangleArea(origine, getVertex(i), getVertex(i+1));            
+        }
+        return area;
     }
-    
+
     private boolean samePoint(Point point1, Point point2) {
         return (point1.x() == point2.x() && point1.y() == point2.y());
     }
-    
-    private boolean isOnTheLeft (Point p, Point a, Point b){
-        return  (signedTriangleArea(p, a, b)>0);
+
+    private boolean isOnTheLeft(Point p, Point a, Point b) {
+        return (signedTriangleArea(p, a, b) > 0);
     }
-    
-    private double signedTriangleArea(Point a, Point b, Point c){
-        return 0.5*((b.x()- a.x())*(c.y()-a.y())-(c.x()-a.x())*(b.y()-a.y()));
+
+    private double signedTriangleArea(Point a, Point b, Point c) {
+        return 0.5 * ((b.x() - a.x()) * (c.y() - a.y()) - (c.x() - a.x())
+                * (b.y() - a.y()));
     }
-    
+
     private Point getVertex(int n) {
         return points().get(Math.floorMod(n, points().size()));
     }
-    
-    public boolean containsPoint(Point p){
-        return false;
+
+    public boolean containsPoint(Point p) {
+        int index = 0;
+        for (int i = 0; i <= points().size(); i++) {
+            if (getVertex(i).y() <= p.y()) {
+                if (getVertex(i + 1).y() > p.y()) {
+                    index++;
+                }
+            } else {
+                if (getVertex(i + 1).y() <= p.y()
+                        && isOnTheLeft(p, getVertex(i + 1), getVertex(i))) {
+                    index--;
+                }
+            }
         }
+        return index != 0;
+    }
 }
