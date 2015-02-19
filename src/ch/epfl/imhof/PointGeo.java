2b059e219e635861d800093bcd14a40c566b540e
Nicolas <panv@hotmail.fr>
2015-02-19 19:21:26 +0100

    Documentation, encore et toujours + mise en forme étape 1 terminée?

--- src/ch/epfl/imhof/PointGeo.java
+++ src/ch/epfl/imhof/PointGeo.java
@@ -5,13 +5,25 @@ package ch.epfl.imhof;
  * caractérisé par une latitude et par une longitude. On fournit deux méthodes
  * d'accès aux attributs. La classe est immuable.
  * 
- * @author Vincenzo Bazzucchi (249733), Nicolas Phan Van (239293)
+ * @author Vincenzo Bazzucchi (249733)
+ * @author Nicolas Phan Van (239293)
  *
  */
 public final class PointGeo {
     private final double longitude;
     private final double latitude;
 
+    /**
+     * Construit un point avec les coordonnées données
+     * 
+     * @param longitude
+     *            la longitude du point, en radians
+     * @param latitude
+     *            la latitude du point, en radians
+     * @throws IllegalArgumentException
+     *             lève une exception si la latitude ou la longitude fournies
+     *             sont hors des intervalles autorisés
+     */
     public PointGeo(double longitude, double latitude)
             throws IllegalArgumentException {
         try {
@@ -25,22 +37,42 @@ public final class PointGeo {
             }
             this.longitude = longitude;
             this.latitude = latitude;
-        }
-        catch (IllegalArgumentException e) {
+        } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             throw e;
         }
     }
 
+    /**
+     * Getter pour la longitude
+     * 
+     * @return l'attribut longitude du point
+     */
     public double longitude() {
         return longitude;
     }
 
+    /**
+     * Getter pour la latitude
+     * 
+     * @return l'attribut latitude du point
+     */
     public double latitude() {
         return latitude;
     }
 
+    /**
+     * Teste si la coordonnée est valide
+     * 
+     * @param coordonnee
+     *            coordonnée à tester, en radians
+     * @param parametre
+     *            modificateur d'intervalle, 1.0 pour la longitude, 2.0 pour la
+     *            latitude
+     * @return false si la coordonée est valide, true sinon
+     */
     private boolean testCoordonnee(double coordonnee, double parametre) {
-        return (coordonnee < -Math.PI / parametre || coordonnee > Math.PI / parametre);
+        return (coordonnee < -Math.PI / parametre || coordonnee > Math.PI
+                / parametre);
     }
 }
--- src/ch/epfl/imhof/geometry/Point.java
+++ src/ch/epfl/imhof/geometry/Point.java
@@ -5,22 +5,41 @@ package ch.epfl.imhof.geometry;
  * caractérisé par une coordonnée horizontale x et une coordonnée verticale y.
  * On fournit deux méthodes d'accès aux attributs. La classe est immuable.
  * 
- * @author Vincenzo Bazzucchi (249733), Nicolas Phan Van (239293)
+ * @author Vincenzo Bazzucchi (249733)
+ * @author Nicolas Phan Van (239293)
  * 
  */
 public final class Point {
     private final double x;
     private final double y;
 
+    /**
+     * Construit un point avec les coordonnées données
+     * 
+     * @param x
+     *            la coordonnée horizontale du point
+     * @param y
+     *            la coordonnée verticale du point
+     */
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Getter pour la coordonnée horizontale
+     * 
+     * @return l'attribut x du point
+     */
     public double x() {
         return x;
     }
 
+    /**
+     * Getter pour la coordonnée verticale
+     * 
+     * @return l'attribut y du point
+     */
     public double y() {
         return y;
     }
