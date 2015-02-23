43c1c7bd89b30989cf4123252eeb37462a8dafbf
Nicolas <phanvan@localhost>
2015-02-23 14:51:32 +0100

    Javadoc pour la classe Attributes

--- src/ch/epfl/imhof/Attributes.java
+++ src/ch/epfl/imhof/Attributes.java
@@ -1,28 +1,69 @@
 package ch.epfl.imhof;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+
+/**
+ * 
+ * @author Vincenzo Bazzucchi (249733)
+ * @author Nicolas Phan Van (239293)
+ *
+ */
 public final class Attributes {
     private HashMap<String, String> attributes;
     
+    /**
+     * Construit un nouvel ensemble d'attributs à partir de la table passée en paramètre
+     *  
+     * @param attributes les attributs, sous forme de Map de String
+     */
     public Attributes(Map<String, String> attributes) {
         this.attributes = new HashMap(Map<String, String> attributes);
     }
     
+    /**
+     * 
+     * @return true si l'ensemble d'attributs est vide, false sinon
+     */
     public boolean isEmpty() {
         return attributes.isEmpty();
     }
     
+    /**
+     * 
+     * @param key la clé dont on cherche l'appartenance à l'ensemble d'attributs, sous forme de String
+     * @return true si la clé appartient à la liste d'attributs, false sinon 
+     */
     public boolean contains(String key) {
         return (attributes.containsKey(key) || attributes.containsValue(key));
     }
     
+    /**
+     * 
+     * @param key la clé dont on cherche la valeur associée, sous forme de String
+     * @return la valeur associée à la clé passée en paramètre si elle existe, null sinon
+     */
     public String get(String key) {
         return attributes.get(key);
     }
     
+    /**
+     * 
+     * @param key la clé dont on cherche la valeur associée, sous forme de String
+     * @param defaultValue la valeur par défaut, une String
+     * @return la valeur associée à la clé passée en paramètre, la valeur par défaut si aucune valeur ne lui est associé
+     */
     public String get(String key, String defaultValue) {
         return attributes.getOrDefault(key, defaultValue);
     }
     
+    /**
+     * 
+     * @param key la clé dont on cherche l'entier associé, sous forme de String
+     * @param defaultValue la valeur par défaut, un int
+     * @return l'entier associé à la clé passée en paramètre s'il existe, la valeur par défaut sinon
+     */
     public int get(String key, int defaultValue) {
         int temp;
         try {
@@ -35,6 +76,12 @@ public final class Attributes {
         }
     }
     
+    /**
+     * Renvoie une version filtrée des attributs, ne contenant que ceux contenus dans l'ensemble fourni en paramètre
+     * 
+     * @param keysToKeep les valeurs qu'on veut garder
+     * @return un Attributes, filtré
+     */
     public Attributes keepOnlyKeys(Set<String> keysToKeep) {
         Attributes filteredAttributes = new Builder();
         for (int i = 0; i < keysToKeep.size(); ++i) {
@@ -45,13 +92,31 @@ public final class Attributes {
         return filteredAttributes.build();
     }
     
+    /**
+     * Bâtisseur de la classe Attributes
+     * 
+     * @author Vincenzo Bazzucchi (249733)
+     * @author Nicolas Phan Van (239293)
+     *
+     */
     public static final class Builder {
         private HashMap<String, String> attributes;
         
+        /**
+         * Ajoute le couple clé, valeur à l'Attributes en construction
+         * 
+         * @param key la clé, une String
+         * @param value la valeur, une String
+         */
         public void put(String key, String value) {
             attributes.put(key, value);
         }
         
+        /**
+         * Construit le nouvel Attributes
+         * 
+         * @return 
+         */
         public Attributes build() {
             return new Attributes(attributes);
         }
