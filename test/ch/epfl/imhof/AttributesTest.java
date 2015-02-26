b08c71e44bf4ed7fab1955c78f36dbe4f48d67d5
Vincenzo <vincenzo.bazzucchi@epfl.ch>
2015-02-26 11:18:21 +0100

    On a mélangé nos tests (tes tests avec mes  deux ridicules lignes de code). Good Job!

--- test/ch/epfl/imhof/AttributesTest.java
+++ test/ch/epfl/imhof/AttributesTest.java
@@ -1,34 +1,5 @@
 package ch.epfl.imhof;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import java.util.Map;
-import java.util.HashMap;
-import org.junit.Test;
-
-public class AttributesTest {
-    @Test
-    public void isEmptyTest() {
-        Map<String, String> emptyMap = new HashMap<String,String>();
-        assertTrue(emptyMap.isEmpty());
-        Map<String, String> notEmptyMap = new HashMap<String,String>();
-        notEmptyMap.put("nature", "forest");
-        assertFalse(notEmptyMap.isEmpty());  
-    }
-    
-    @Test
-    public void containsTest(){
-        Map<String, String> notEmptyMap = new HashMap<String,String>();
-        notEmptyMap.put("nature", "forest");
-        assertTrue(notEmptyMap.contains("nature", "forest"));  
-        
-    }
-    
-    
-}
-package ch.epfl.imhof;
-
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -45,34 +16,50 @@ public class AttributesTest {
         }
         return newHashMap;
     }
-    
+
     private Attributes newAttributes(String... args) {
         return new Attributes(newHashMap(args));
     }
-    
+
     @Test
     public void returnsTrueForEmptyMap() {
         Attributes test = new Attributes(new HashMap<String, String>());
         assertTrue(test.isEmpty());
     }
-    
+
     @Test
     public void returnsFalseForNotEmptyMap() {
         Attributes test = newAttributes("name", "Léman");
         assertFalse(test.isEmpty());
     }
-    
+
     @Test
     public void returnsTrueForKeyPresent() {
         Attributes test = newAttributes("name", "Léman", "color", "blue");
         assertTrue(test.contains("name"));
         assertTrue(test.contains("color"));
     }
-    
+
     @Test
     public void returnsFalseForKeyNotPresent() {
         Attributes test = newAttributes("name", "Léman", "color", "blue");
         assertFalse(test.contains("jambon"));
         assertFalse(test.contains("pourquoi?"));
     }
+
+    @Test
+    public void isEmptyTest() {
+        Map<String, String> emptyMap = new HashMap<String, String>();
+        assertTrue(emptyMap.isEmpty());
+        Map<String, String> notEmptyMap = new HashMap<String, String>();
+        notEmptyMap.put("nature", "forest");
+        assertFalse(notEmptyMap.isEmpty());
+    }
+
+    @Test
+    public void containsTest() {
+        Attributes notEmpty = newAttributes("nature", "forest", "highways", "motorway");
+        assertTrue(notEmpty.contains( "forest"));
+    }
+
 }
