966491e0860a2c995cf894d2349c499cf7eb024a
Vincenzo <vincenzo.bazzucchi@epfl.ch>
2015-03-04 09:53:25 +0100

    J'ai commencé à écrire ces test quand j'ai vu qu'il était temps d'aller en cours. À toute!

---
+++ test/ch/epfl/imhof/osm/OSMEntityTest.java
@@ -0,0 +1,10 @@
+package ch.epfl.imhof.osm;
+/**
+ * je teste les méthodes de OSMEntity en les appelant à partir des sousclasses
+ * @author Vincenzo Bazzucchi (249733)
+ * @author Nicolas Phan Van (239293)
+ *
+ */
+public class OSMEntityTest {
+
+}
