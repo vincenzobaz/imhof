619c29b536b07bc7e2b02728b0346d7d16c95641
Nicolas <panv@hotmail.fr>
2015-03-10 15:27:28 +0100

    Javadoc de la classe Graph

--- src/ch/epfl/imhof/Graph.java
+++ src/ch/epfl/imhof/Graph.java
@@ -6,9 +6,26 @@ import java.util.HashMap;
 import java.util.Set;
 import java.util.HashSet;
 
+/**
+ * Classe représentant un graphe non-orienté. Elle est immuable et générique,
+ * son paramètre représente le type des noeuds du graphe.
+ * 
+ * @author Vincenzo Bazzucchi (249733)
+ * @author Nicolas Phan Van (239293)
+ *
+ * @param <N>
+ *            le type des noeuds du graphe
+ */
 public final class Graph<N> {
     private final Map<N, Set<N>> neighbors;
 
+    /**
+     * Construit un nouveau graphe à partir de la table d'adjacence passée en
+     * paramètre
+     * 
+     * @param neighbors
+     *            la table d'adjacence du graphe, une Map
+     */
     public Graph(Map<N, Set<N>> neighbors) {
         Map<N, Set<N>> temp = new HashMap<>();
         for (Map.Entry<N, Set<N>> m : neighbors.entrySet()) {
@@ -18,10 +35,25 @@ public final class Graph<N> {
         this.neighbors = Collections.unmodifiableMap(new HashMap<>(temp));
     }
 
+    /**
+     * Retourne l'ensemble des noeuds du graphe
+     * 
+     * @return les noeuds du graphe, sous forme de Set
+     */
     public Set<N> nodes() {
         return neighbors.keySet();
     }
 
+    /**
+     * Retourne l'ensemble des noeuds voisins du noeud donné
+     * 
+     * @param node
+     *            le noeud dont on cherche les voisins
+     * @return les voisins du noeud, sous forme de Set
+     * @throws IllegalArgumentException
+     *             lève une exception si le noeud passé en paramètre ne fait pas
+     *             partie du graphe
+     */
     public Set<N> neighborsOf(N node) throws IllegalArgumentException {
         if (!neighbors.containsKey(node)) {
             throw new IllegalArgumentException(
@@ -30,19 +62,50 @@ public final class Graph<N> {
         return neighbors.get(node);
     }
 
+    /**
+     * Bâtisseur de la classe Graph
+     * 
+     * @author Vincenzo Bazzucchi (249733)
+     * @author Nicolas Phan Van (239293)
+     *
+     * @param <N>
+     *            le type des noeuds du graphe en construction
+     */
     public static final class Builder<N> {
         private Map<N, Set<N>> neighbors;
 
+        /**
+         * Constructeur par défaut du bâtisseur
+         */
         public Builder() {
             neighbors = new HashMap<>();
         }
 
+        /**
+         * Ajoute le noeud donné au graphe en cours de construction, s'il n'en
+         * fait pas déjà partie
+         * 
+         * @param n
+         *            le noeud qu'on souhaite ajouter au graphe
+         */
         public void addNode(N n) {
             if (!neighbors.containsKey(n)) {
                 neighbors.put(n, new HashSet<>());
             }
         }
 
+        /**
+         * Ajoute une arête entre les deux noeuds donnés au graphe en cours de
+         * construction
+         * 
+         * @param n1
+         *            le premier noeud de l'arête
+         * @param n2
+         *            le second noeud de l'arête
+         * @throws IllegalArgumentException
+         *             lève une exception si un des noeuds, ou les deux,
+         *             n'appartient pas au graphe en construction
+         */
         public void addEdge(N n1, N n2) throws IllegalArgumentException {
             if (!(neighbors.containsKey(n1) && neighbors.containsKey(n2))) {
                 throw new IllegalArgumentException(
@@ -52,6 +115,12 @@ public final class Graph<N> {
             neighbors.get(n2).add(n1);
         }
 
+        /**
+         * Construit le graphe composé des noeuds et arêtes ajoutés jusqu'à
+         * présent au bâtisseur
+         * 
+         * @return le nouveau graphe
+         */
         public Graph<N> build() {
             return new Graph<>(neighbors);
         }
