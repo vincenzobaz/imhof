74d9e4fe71f07f2511d2d2401add444de4920f3c
Nicolas <panv@hotmail.fr>
2015-03-16 14:03:05 +0100

    COntinuation de OSMToGeomachin

--- src/ch/epfl/imhof/osm/OSMToGeoTransformer.java
+++ src/ch/epfl/imhof/osm/OSMToGeoTransformer.java
@@ -1,27 +1,35 @@
 package ch.epfl.imhof.osm;
 
+import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Set;
-import java.util.HashSet;
 
 import ch.epfl.imhof.Attributes;
 import ch.epfl.imhof.Attributed;
-import ch.epfl.imhof.geometry.ClosedPolyLine;
-import ch.epfl.imhof.geometry.Polygon;
+import ch.epfl.imhof.geometry.*;
 import ch.epfl.imhof.projection.Projection;
 
 public final class OSMToGeoTransformer {
     private final Projection projection;
-    private final static Set<String> closedWayAttributes = new HashSet<>(String[]);
+    private final List<String> closedWayAttributes;
+    private final List<String> polyLineAttributes;
+    private final List<String> polygonAttributes;
 
     public OSMToGeoTransformer(Projection projection) {
-        String[] temp = {"aeroway", "amenity", "building", "harbour", "historic",
+        String[] tab1 = {"aeroway", "amenity", "building", "harbour", "historic",
                 "landuse", "leisure", "man_made", "military", "natural",
                 "office", "place", "power", "public_transport", "shop",
                 "sport", "tourism", "water", "waterway", "wetland"};
-        this.projection = projection;
+        String[] tab2 = {"bridge", "highway", "layer", "man_made", "railway",
+                "tunnel", "waterway"};
+        String[] tab3 = {"building", "landuse", "layer", "leisure", "natural",
+                "waterway"};
         
+        closedWayAttributes = new ArrayList(Arrays.asList(tab1));
+        polyLineAttributes = new ArrayList<>(Arrays.asList(tab2));
+        polygonAttributes = new ArrayList<>(Arrays.asList(tab3));
+        this.projection = projection;
     }
 
     public Map transform(OSMMap map) {
@@ -29,11 +37,33 @@ public final class OSMToGeoTransformer {
     }
 
     private List<ClosedPolyLine> ringsForRole(OSMRelation relation, String role) {
-
+        List<ClosedPolyLine> ringsforRole = new ArrayList<>();
+        List<OSMRelation.Member> relationMembers = relation.members();
+        for (OSMRelation.Member member : relationMembers) {
+            if (member.role().equals(role)) {
+                ringsforRole.add(arg0)
+            }
+        }
     }
 
     private List<Attributed<Polygon>> assemblePolygon(OSMRelation relation,
             Attributes attributes) {
 
     }
+    
+    private PolyLine OSMWayProjection (OSMWay way) {
+        PolyLine.Builder polylineInConstruction = new PolyLine.Builder();
+        List<OSMNode> nodesList = way.nonRepeatingNodes();
+        
+        for (OSMNode nodeToAdd : nodesList) {
+            polylineInConstruction.addPoint(projection.project(nodeToAdd.position()));
+        }
+        if (way.isClosed()) {
+            return polylineInConstruction.buildClosed();
+        } else {
+            return polylineInConstruction.buildOpen();
+        }
+    }
+    
+    
 }
