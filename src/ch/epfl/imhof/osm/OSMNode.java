57440f8a3f080e5c6115fb69db7f3e29698b221f
Nicolas <panv@hotmail.fr>
2015-03-30 14:05:02 +0200

    révision doc osmnode

--- src/ch/epfl/imhof/osm/OSMNode.java
+++ src/ch/epfl/imhof/osm/OSMNode.java
@@ -4,7 +4,8 @@ import ch.epfl.imhof.PointGeo;
 import ch.epfl.imhof.Attributes;
 
 /**
- *Classe représentant un noeud OSM, héritant de OSMEntity. Elle est immuable.
+ * Classe représentant un noeud OSM, héritant de <code>OSMEntity</code>. Elle
+ * est immuable.
  * 
  * @author Vincenzo Bazzucchi (249733)
  * @author Nicolas Phan Van (239293)
@@ -18,11 +19,11 @@ public final class OSMNode extends OSMEntity {
      * donnés.
      * 
      * @param id
-     *            l'identifiant du noeud, un long
+     *            l'identifiant du noeud, un <code>long</code>
      * @param position
-     *            la position du noeud, un PointGeo
+     *            la position du noeud, un <code>PointGeo</code>
      * @param attributes
-     *            les attributs du noeud, un Attributes
+     *            les attributs du noeud, un <code>Attributes</code>
      */
     public OSMNode(long id, PointGeo position, Attributes attributes) {
         super(id, attributes);
@@ -32,15 +33,15 @@ public final class OSMNode extends OSMEntity {
     /**
      * Retourne la position du noeud.
      * 
-     * @return la position du noeud, un PointGeo
+     * @return la position du noeud, un <code>PointGeo</code>
      */
     public PointGeo position() {
         return position;
     }
 
     /**
-     * Bâtisseur de la classe OSMNode. Il hérite du bâtisseur de la classe
-     * OSMEntity.
+     * Bâtisseur de la classe <code>OSMNode</code>. Il hérite du bâtisseur de la
+     * classe <code>OSMEntity</code>.
      * 
      * @author Vincenzo Bazzucchi (249733)
      * @author Nicolas Phan Van (239293)
@@ -54,9 +55,9 @@ public final class OSMNode extends OSMEntity {
          * position donnés.
          * 
          * @param id
-         *            l'identifiant du noeud à bâtir, un long
+         *            l'identifiant du noeud à bâtir, un <code>long</code>
          * @param position
-         *            la position du noeud à bâtir, un PointGeo
+         *            la position du noeud à bâtir, un <code>PointGeo</code>
          */
         public Builder(long id, PointGeo position) {
             super(id);
@@ -67,7 +68,7 @@ public final class OSMNode extends OSMEntity {
          * Construit et retourne un noeud OSM avec l'identifiant et la position
          * passés au constructeur, et les attributs ajoutés par le bâtisseur.
          * 
-         * @return le noeud, un OSMNode
+         * @return le noeud, un <code>OSMNode</code>
          * @throws IllegalStateException
          *             lève une exception si le noeud est incomplet (si la
          *             méthode setIncomplete a été appelée sur le bâtisseur)
