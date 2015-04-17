89eaa44b234fd1268b4004411d44aea3a591f811
Nicolas <panv@hotmail.fr>
2015-04-18 01:08:12 +0200

    doc dans java2Dcanvas, reste juste à faire le ménage

--- src/ch/epfl/imhof/painting/Java2DCanvas.java
+++ src/ch/epfl/imhof/painting/Java2DCanvas.java
@@ -14,6 +14,14 @@ import ch.epfl.imhof.geometry.PolyLine;
 import ch.epfl.imhof.geometry.ClosedPolyLine;
 import ch.epfl.imhof.geometry.Polygon;
 
+/**
+ * Classe représentant une toile. Elle permet de dessiner des polylignes et des
+ * polygones dans une image discrète.
+ * 
+ * @author Vincenzo Bazzucchi (249733)
+ * @author Nicolas Phan Van (239293)
+ *
+ */
 public final class Java2DCanvas implements Canvas {
     // pas besoin de tous ces trucs en fait
     private Point bottomLeft;
@@ -28,6 +36,29 @@ public final class Java2DCanvas implements Canvas {
     private final BufferedImage image;
     private final Graphics2D context;
 
+    /**
+     * Construit une toile ayant pour coins les deux points fournis, la largeur,
+     * la hauteur et la résolution données, et pour couleur de fond la couleur
+     * donnée.
+     * 
+     * @param bottomLeft
+     *            le coin bas-gauche de la toile
+     * @param topRight
+     *            le coin haut-droite de la toile
+     * @param width
+     *            la largeur de la toile
+     * @param height
+     *            l hauteur de la toile
+     * @param dpi
+     *            la résolution de la toile
+     * @param backgroundColor
+     *            la couleur de fond de la toile
+     * @throws IllegalArgumentException
+     *             lève une exception si la largeur, la hauteur ou la résolution
+     *             sont négatives ou nulles, ou si le premier point se trouve
+     *             plus en haut ou plus à doite que le deuxième point
+     */
+    // changer le type de width, height et dpi en float?
     public Java2DCanvas(Point bottomLeft, Point topRight, int width,
             int height, int dpi, Color backgroundColor)
             throws IllegalArgumentException {
@@ -48,9 +79,9 @@ public final class Java2DCanvas implements Canvas {
         double scale = dpi / 72.0;
         this.basisChange = Point.alignedCoordinateChange(bottomLeft, new Point(
                 0d, height * scale), topRight, new Point(width * scale, 0d));
-        
+
         image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        
+
         context = image.createGraphics();
         context.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                 RenderingHints.VALUE_ANTIALIAS_ON);
@@ -59,6 +90,11 @@ public final class Java2DCanvas implements Canvas {
         context.fillRect(0, 0, width, height);
     }
 
+    /**
+     * Retourne l'image dessinée par la toile
+     * 
+     * @return l'image
+     */
     public BufferedImage image() {
         return image;
     }
@@ -96,6 +132,15 @@ public final class Java2DCanvas implements Canvas {
         context.fill(area);
     }
 
+    /**
+     * Retoune la valeur entière correspondant au type de terminaison de la
+     * polyligne.
+     * 
+     * @param capStyle
+     *            la terminaison de la polyligne
+     * @return l'entier correspondant à la terminaison dans la classe
+     *         <code>BasicStroke</code>
+     */
     private int cap(LineStyle.LineCap capStyle) {
         switch (capStyle) {
         case BUTT:
@@ -109,6 +154,15 @@ public final class Java2DCanvas implements Canvas {
         }
     }
 
+    /**
+     * Retourne la valeur entière correspondant au type de jointure entre deux
+     * segments.
+     * 
+     * @param joinStyle
+     *            la jointure
+     * @return l'entier correspondant à la jointure dans la classe
+     *         <code>BasicStroke</code>
+     */
     private int join(LineStyle.LineJoin joinStyle) {
         switch (joinStyle) {
         case BEVEL:
