93499d4e5a2cfd265ceb269e5de799f8cda34f8c
Nicolas <panv@hotmail.fr>
2015-04-25 19:35:58 +0200

    déplacé roadspec à l'intérieur de roadpaintergenerator

--- src/ch/epfl/imhof/painting/RoadPainterGenerator.java
+++ src/ch/epfl/imhof/painting/RoadPainterGenerator.java
@@ -1,5 +1,8 @@
 package ch.epfl.imhof.painting;
 
+import java.util.function.Predicate;
+
+import ch.epfl.imhof.Attributed;
 import ch.epfl.imhof.painting.LineStyle.LineCap;
 import ch.epfl.imhof.painting.LineStyle.LineJoin;
 
@@ -30,5 +33,57 @@ public final class RoadPainterGenerator {
 
         return intBridgePainter.above(coBridgePainter.above(intBridgePainter
                 .above(coNormRoadPainter.above(tunnelPainter))));
+    }
+
+    public final static class RoadSpec {
+        private final Predicate<Attributed<?>> filter;
+        private final float wI;
+        private final Color cI;
+        private final float wC;
+        private final Color cC;
+
+        public RoadSpec(Predicate<Attributed<?>> filter, float wI, Color cI,
+                float wC, Color cC) {
+            this.filter = filter;
+            this.wI = wI;
+            this.cI = cI;
+            this.wC = wC;
+            this.cC = cC;
+        }
+
+        /**
+         * @return the filter
+         */
+        public Predicate<Attributed<?>> getFilter() {
+            return filter;
+        }
+
+        /**
+         * @return the wI
+         */
+        public float getwI() {
+            return wI;
+        }
+
+        /**
+         * @return the cI
+         */
+        public Color getcI() {
+            return cI;
+        }
+
+        /**
+         * @return the wC
+         */
+        public float getwC() {
+            return wC;
+        }
+
+        /**
+         * @return the cC
+         */
+        public Color getcC() {
+            return cC;
+        }
     }
 }
