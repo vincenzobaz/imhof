fdb5573505673f9b53d88b2e5d1e2667acab5989
phanvan <phanvan@localhost>
2015-04-29 15:02:49 +0200

    rien fait

--- src/ch/epfl/imhof/dem/HGTDigitalElevationModel.java
+++ src/ch/epfl/imhof/dem/HGTDigitalElevationModel.java
@@ -1,12 +1,17 @@
 package ch.epfl.imhof.dem;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.FileChannel.MapMode;
 
-public class HGTDigitalElevationModel {
+import ch.epfl.imhof.PointGeo;
+import ch.epfl.imhof.Vector3D;
+
+public class HGTDigitalElevationModel implements DigitalElevationModel {
     private final MappedByteBuffer model;
 
     public HGTDigitalElevationModel(File model) throws FileNotFoundException {
@@ -14,4 +19,19 @@ public class HGTDigitalElevationModel {
 
     }
 
+    public Vector3D normalAt(PointGeo point) throws IllegalArgumentException {
+        if (point.latitude() < latitude || point.latitude() > latitude
+                || point.longitude() < longitude
+                || point.longitude() > longitude) {
+            throw new IllegalArgumentException(
+                    "Le point fourni ne fait pas partie de la zone couverte par le MNT.");
+        }
+        
+        double s = Earth.RADIUS * HGTResolution;
+        Vector3D a = new Vector3D(s, 0d, );
+        Vector3D b = new Vector3D(0d, s, z);
+        Vector3D c = new Vector3D(-s, 0d, );
+        Vector3D d = new Vector3D(0, -s );
+        
+    }
 }
