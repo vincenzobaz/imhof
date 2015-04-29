3cedb813f9724a7132d24aacee9e92ae43ae6a24
phanvan <phanvan@localhost>
2015-04-29 13:13:32 +0200

    modifié projection pour stocker les variables intermédiaires et enlevé les math.pow

--- src/ch/epfl/imhof/projection/CH1903Projection.java
+++ src/ch/epfl/imhof/projection/CH1903Projection.java
@@ -26,11 +26,15 @@ public final class CH1903Projection implements Projection {
     public Point project(PointGeo point) {
         double longitude = (Math.toDegrees(point.longitude()) * 3600 - 26782.5) / 10000;
         double latitude = (Math.toDegrees(point.latitude()) * 3600 - 169028.66) / 10000;
-        double x = 600072.37 + 211455.93 * longitude - 10938.51 * longitude * latitude - 0.36
-                * longitude * Math.pow(latitude, 2) - 44.54 * Math.pow(longitude, 3);
-        double y = 200147.07 + 308807.95 * latitude + 3745.25 * Math.pow(longitude, 2)
-                + 76.63 * Math.pow(latitude, 2) - 194.56 * Math.pow(longitude, 2) * latitude
-                + 119.79 * Math.pow(latitude, 3);
+        double longitudeSquared = longitude * longitude;
+        double latitudeSquared = latitude * latitude;
+        double x = 600072.37 + 211455.93 * longitude - 10938.51 * longitude
+                * latitude - 0.36 * longitude * latitudeSquared - 44.54
+                * longitudeSquared * longitude;
+        double y = 200147.07 + 308807.95 * latitude + 3745.25
+                * longitudeSquared + 76.63 * latitudeSquared - 194.56
+                * longitudeSquared * latitude + 119.79 * latitudeSquared
+                * latitude;
         return new Point(x, y);
     }
 
@@ -45,11 +49,13 @@ public final class CH1903Projection implements Projection {
     public PointGeo inverse(Point point) {
         double x1 = (point.x() - 600000) / 1000000;
         double y1 = (point.y() - 200000) / 1000000;
+        double x1Squared = x1 * x1;
+        double y1Squared = y1 * y1;
         double lambda0 = 2.6779094 + 4.728982 * x1 + 0.791484 * x1 * y1
-                + 0.1306 * x1 * Math.pow(y1, 2) - 0.0436 * Math.pow(x1, 3);
-        double phi0 = 16.9023892 + 3.238272 * y1 - 0.270978 * Math.pow(x1, 2)
-                - 0.002528 * Math.pow(y1, 2) - 0.0447 * Math.pow(x1, 2) * y1
-                - 0.0140 * Math.pow(y1, 3);
+                + 0.1306 * x1 * y1Squared - 0.0436 * x1Squared * x1;
+        double phi0 = 16.9023892 + 3.238272 * y1 - 0.270978 * x1Squared
+                - 0.002528 * y1Squared - 0.0447 * x1Squared * y1 - 0.0140
+                * y1Squared * y1;
         return new PointGeo(Math.toRadians(lambda0 * 100 / 36),
                 Math.toRadians(phi0 * 100 / 36));
     }
