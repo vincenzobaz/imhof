2f33466cfc7fb1e424078ba50b1e2347d8d4d0df
Nicolas <panv@hotmail.fr>
2015-05-01 12:49:57 +0200

    modifs doc linestyle

--- src/ch/epfl/imhof/painting/LineStyle.java
+++ src/ch/epfl/imhof/painting/LineStyle.java
@@ -1,7 +1,8 @@
 package ch.epfl.imhof.painting;
 
 /**
- * Classe regroupant tous les paramètres de style utiles au dessin d'une ligne
+ * Classe regroupant tous les paramètres de style utiles au dessin d'une ligne.
+ * Elle est immuable.
  * 
  * @author Vincenzo Bazzucchi (249733)
  * @author Nicolas Phan Van (239293)
@@ -15,23 +16,23 @@ public final class LineStyle {
     private final float[] dashingPattern;
 
     /**
-     * Construit un style de ligne à partir des cinq paramètres de style
+     * Construit un style de ligne à partir des cinq paramètres de style donnés.
      * 
      * @param width
-     *            largeur
+     *            la largeur du trait
      * @param color
-     *            couleur
+     *            la couleur du trait
      * @param cap
-     *            type de terminaison
+     *            le type de terminaison des lignes
      * @param join
-     *            type de jointure
+     *            le type de jointure des segments
      * @param dashingPattern
-     *            alternance des sections opaques et transparentes pour le
+     *            l'alternance des sections opaques et transparentes pour le
      *            dessin en traitillés
      * @throws IllegalArgumentException
-     *             lancée si la largeur du trait est négative ou si l'un des
-     *             éléments de la séquence d'alternance des segments est négatif
-     *             ou nul
+     *             lève une exception si la largeur du trait est négative ou si
+     *             l'un des éléments de la séquence d'alternance des segments
+     *             est négatif ou nul
      */
     public LineStyle(float width, Color color, LineCap cap, LineJoin join,
             float... dashingPattern) throws IllegalArgumentException {
@@ -44,6 +45,9 @@ public final class LineStyle {
         this.color = color;
         this.cap = cap;
         this.join = join;
+
+        // Test de validité des éléments du tableau passé en argument et copie
+        // de celui-ci
         if (dashingPattern == null) {
             this.dashingPattern = dashingPattern;
         } else {
@@ -61,125 +65,133 @@ public final class LineStyle {
     }
 
     /**
-     * Constructeur secondaire prenant seulement deux arguments et qui appelle
-     * le constructeur principal en lui fournissant des valeurs par défaut
+     * Constructeur secondaire prenant seulement en arguments la largeur et la
+     * couleur du trait, et utilisant des valeurs par défaut pour les autres
+     * paramètres. Les valeurs par défaut sont: butt pour la terminaison des
+     * lignes, miter pour la jointure des segments, et un trait continu.
      * 
      * @param width
-     *            largeur
+     *            la largeur du trait
      * @param color
-     *            couleur
+     *            la couleur du trait
      */
-    public LineStyle(float width, Color color) {
+    public LineStyle(float width, Color color) throws IllegalArgumentException {
         this(width, color, LineCap.BUTT, LineJoin.MITER, new float[0]);
     }
 
     /**
+     * Retourne la largeur du trait.
      * 
-     * @return la largeur de la ligne
+     * @return la largeur
      */
     public float width() {
         return width;
     }
 
     /**
+     * Retourne la couleur du trait.
      * 
-     * @return la couleur de la ligne, un objet de type
-     *         <code> ch.epfl.imhof.painting.Color </code>
+     * @return la couleur, sous forme de
+     *         <code>ch.epfl.imhof.painting.Color</code>
      */
     public Color color() {
         return color;
     }
 
     /**
+     * Retourne le type de terminaison de la ligne.
      * 
-     * @return le type de terminaison de la ligne.
+     * @return la terminaison
      */
     public LineCap cap() {
         return cap;
     }
 
     /**
+     * Retourne le type de jointure des segments.
      * 
-     * @return le type de jointure de la ligne
+     * @return le type de jointure
      */
     public LineJoin join() {
         return join;
     }
 
     /**
+     * Retourne la séquence d'alternance des sections opaques et transparentes
+     * pour le dessin en traitillés.
      * 
-     * @return l'alternance des segments opaques et transparents, un tableau de
-     *         <code> int </code>.
-     * */
+     * @return l'alternance des segments, un tableau de <code>float</code>.
+     */
     public float[] dashingPattern() {
-        // la classe étant immuable et les tableaux statiques java ne pouvant
-        // pas être rendus imuables, on retourne une copie défensive du tableau.
+        // La classe étant immuable et les tableaux statiques java ne pouvant
+        // pas être rendus immuables, on retourne une copie défensive du
+        // tableau.
         return dashingPattern == null ? null : dashingPattern.clone();
     }
 
     /**
-     * Permet d'obtenir un nouveau style de ligne à partir de
-     * <code> this </code> différant seulement pour la largeur du trait.
+     * Retourne un nouveau style de ligne différant de <code>this</code> par la
+     * largeur du trait.
      * 
      * @param width
-     *            largeur de la ligne
-     * @return un nouveau <code> LineStyle </code>
+     *            la nouvelle largeur du trait
+     * @return un nouveau <code>LineStyle</code>
      */
     public LineStyle withWidth(float width) {
         return new LineStyle(width, color, cap, join, dashingPattern);
     }
 
     /**
-     * Permet d'obtenur un nouveau style de ligne à partir de <code> this</code>
-     * différant du premier seulement pour la couleur du trait
+     * Retourne un nouveau style de ligne différant de <code>this</code> par la
+     * couleur du trait.
      * 
      * @param color
-     *            la couleur du trait
-     * @return un nouveau <code> LineStyle </code>
+     *            la nouvelle couleur du trait
+     * @return un nouveau <code>LineStyle</code>
      */
     public LineStyle withColor(Color color) {
         return new LineStyle(width, color, cap, join, dashingPattern);
     }
 
     /**
-     * Permet d'obtenur un nouveau style de ligne à partir de <code> this</code>
-     * différant du premier seulement pour le type de terminaison de ligne.
+     * Retourne un nouveau style de ligne différant de <code>this</code> par le
+     * type de terminaison de ligne.
      * 
      * @param cap
-     *            le type de terminaison de ligne
-     * @return un nouveau <code> LineStyle </code>
+     *            le nouveau type de terminaison de ligne
+     * @return un nouveau <code>LineStyle</code>
      */
     public LineStyle withCap(LineCap cap) {
         return new LineStyle(width, color, cap, join, dashingPattern);
     }
 
     /**
-     * Permet d'obtenur un nouveau style de ligne à partir de <code> this</code>
-     * différant du premier seulement pour le type de jointure de segment
+     * Retourne un nouveau style de ligne différant de <code>this</code> par le
+     * type de jointure de segments.
      * 
      * @param join
-     *            le type de jointure du segment
-     * @return un nouveau <code> LineStyle </code>
+     *            le nouveau type de jointure de segments
+     * @return un nouveau <code>LineStyle</code>
      */
     public LineStyle withJoin(LineJoin join) {
         return new LineStyle(width, color, cap, join, dashingPattern);
     }
 
     /**
-     * Permet d'obtenur un nouveau style de ligne à partir de <code> this</code>
-     * différant du premier seulement pour l'alternance des segments opaques et
-     * transparents
+     * Retourne un nouveau style de ligne différant de <code>this</code> par
+     * l'alternance des segments opaques et transparents.
      * 
      * @param dashingPattern
-     *            l'alternance de segments opaques et transparents
-     * @return un nouveau <code> LineStyle </code>
+     *            le nouveau schéma d'alternance de segments opaques et
+     *            transparents
+     * @return un nouveau <code>LineStyle</code>
      */
     public LineStyle withDashingPattern(float... dashingPattern) {
         return new LineStyle(width, color, cap, join, dashingPattern);
     }
 
     /**
-     * Énumération des type de terminaison de la ligne.
+     * Énumération des types de terminaison d'une ligne.
      * 
      * @author Vincenzo Bazzucchi (249733)
      * @author Nicolas Phan Van (239293)
@@ -190,7 +202,7 @@ public final class LineStyle {
     }
 
     /**
-     * Énumération des type de jointure de la ligne.
+     * Énumération des types de jointure de segments de ligne.
      * 
      * @author Vincenzo Bazzucchi (249733)
      * @author Nicolas Phan Van (239293)
