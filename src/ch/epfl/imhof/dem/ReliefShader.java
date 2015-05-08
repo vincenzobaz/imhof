e2001f7c9f2bf986a2240819bcd93f2525a92e06
Vincenzo <vincenzo.bazzucchi@epfl.ch>
2015-05-08 13:23:27 +0200

    créé classes pour dernière étape

---
+++ src/ch/epfl/imhof/dem/ReliefShader.java
@@ -0,0 +1,28 @@
+package ch.epfl.imhof.dem;
+
+import java.awt.image.BufferedImage;
+import java.util.function.Function;
+
+import ch.epfl.imhof.Vector3D;
+import ch.epfl.imhof.geometry.Point;
+import ch.epfl.imhof.projection.Projection;
+
+public final class ReliefShader {
+    private final Vector3D light;
+
+    
+    public ReliefShader(Projection projection, HGTDigitalElevationModel model, Vector3D light){
+        
+    }
+    
+    private ReliefShader raw(int width, int height, Function<Point, Point> imageToPlan){
+        
+        
+    }
+    
+    private BufferedImage composeWithImage(BufferedImage image){
+        
+    }
+    
+
+}
