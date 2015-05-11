c317946ca92bf10c1a61e1ed2fe47f847cb1afa7
phanvan <phanvan@localhost>
2015-05-11 14:25:37 +0200

    test inutile

--- test/ch/epfl/imhof/dem/ReliefShaderTest.java
+++ test/ch/epfl/imhof/dem/ReliefShaderTest.java
@@ -27,6 +27,23 @@ public final class ReliefShaderTest {
                         .toRadians(46.2))), projection.project(new PointGeo(
                         Math.toRadians(7.8), Math.toRadians(46.8))), 800, 800,
                 0);
-        ImageIO.write(rawRelief, "png", new File("test_raw_relief_1-11.png"));
+        ImageIO.write(rawRelief, "png", new File("test_raw_relief_final.png"));
+    }
+
+    @Test
+    public void correctlyBlursRelief() throws IllegalArgumentException,
+            IOException {
+        Projection projection = new CH1903Projection();
+        File file = new File("data/N46E007.hgt");
+        HGTDigitalElevationModel simmental = new HGTDigitalElevationModel(file);
+        ReliefShader reliefShader = new ReliefShader(projection, simmental,
+                new Vector3D(-1, 1, 1));
+        BufferedImage rawRelief = reliefShader.shadedRelief(
+                projection.project(new PointGeo(Math.toRadians(7.2), Math
+                        .toRadians(46.2))), projection.project(new PointGeo(
+                        Math.toRadians(7.8), Math.toRadians(46.8))), 800, 800,
+                50);
+        ImageIO.write(rawRelief, "png", new File(
+                "test_blurred_relief_final.png"));
     }
 }
