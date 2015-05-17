0eb20209a5937f3a8b99fe465f453e8ca728b0b1
Vincenzo <vincenzo.bazzucchi@epfl.ch>
2015-05-17 15:38:52 +0200

    la grille est dessinnée. Il faut seulement eviter de commencer par le bord mais par le premier point...... l'histoire dont on parlait l'autre jour.

--- src/ch/epfl/imhof/BufferedMapDecorator.java
+++ src/ch/epfl/imhof/BufferedMapDecorator.java
@@ -1,41 +1,54 @@
 package ch.epfl.imhof;
 
-import java.awt.BasicStroke;    
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.font.TextAttribute;
 import java.awt.geom.Line2D;
+import java.awt.geom.Line2D.Double;
 import java.awt.image.BufferedImage;
 
 public class BufferedMapDecorator extends BufferedMap {
     static int howManySquares = 5;
-    
-    public BufferedMapDecorator(BufferedImage map){
+
+    public BufferedMapDecorator(BufferedImage map) {
         super(map);
     }
-    
-    public void addGrid(PointGeo BL, PointGeo TR, int resolution){
+
+    public void addGrid(PointGeo BL, PointGeo TR, int resolution) {
         int width = this.image().getWidth();
         int height = this.image().getHeight();
 
         Graphics2D canvas = this.image().createGraphics();
+        canvas.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
         canvas.setColor(Color.BLACK);
-        float lineWidth = (float) resolution / 254f;
+        // float lineWidth = (float) resolution / 254f;
+        float lineWidth = (float) 0.05d * width / 100;
         BasicStroke line = new BasicStroke(lineWidth);
         canvas.setStroke(line);
-        double radiansPerPixel =  (TR.longitude() - BL.longitude()) / ((double) width);
 
-        int squareSizePixel = (int) Math.round(width /  ((double) howManySquares));
+        double radiansPerPixel = (TR.longitude() - BL.longitude())
+                / ((double) width);
+        System.out.println(radiansPerPixel);
+
+        int squareSizePixel = (int) Math.round(width
+                / ((double) howManySquares));
+        System.out.println(squareSizePixel);
         double squareSizeRadian = radiansPerPixel * squareSizePixel;
-        double longFirstVerticalLine = (Math.abs(BL.longitude()) % squareSizeRadian) + BL.longitude();
-        int x = (int) Math.round(longFirstVerticalLine / radiansPerPixel);
+        System.out.println(squareSizeRadian);
         
-        while (x <= TR.longitude()){
+        canvas.setFont(new Font("inconsolata", Font.PLAIN, 30));
+        for (int x = squareSizePixel; x < width; x += squareSizePixel) {
             canvas.draw(new Line2D.Double(x, 0, x, height - 1));
-            x += squareSizePixel;
+            canvas.drawString("" + (Math.toDegrees(BL.longitude() + squareSizeRadian)), x, 100);
         }
-        
-        
-        // canvas.draw(newPath(line))
-        
+
+        for (int y = squareSizePixel; y < height; y += squareSizePixel) {
+            canvas.draw(new Line2D.Double(0, y, width - 1, y));
+            canvas.drawString("" + Math.toDegrees(BL.latitude() + squareSizeRadian), 0, y);
+        }
     }
 }
