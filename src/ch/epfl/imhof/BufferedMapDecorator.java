cea638cac05810262831396718cf53dc38e5c61f
Vincenzo <vincenzo.bazzucchi@epfl.ch>
2015-05-22 15:19:26 +0200

    addGrid devrait être finie

--- src/ch/epfl/imhof/BufferedMapDecorator.java
+++ src/ch/epfl/imhof/BufferedMapDecorator.java
@@ -7,6 +7,10 @@ import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.geom.Line2D;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class BufferedMapDecorator {
     private final int frameSize;
@@ -37,66 +41,61 @@ public class BufferedMapDecorator {
                 map.getHeight(), null);
     }
 
-    public void addGrid(PointGeo BL, PointGeo TR, int resolution, int howManyCells) {
-
-        int imageWidth = map.getWidth()-2*frameSize;
-        int imageHeight = map.getHeight()-2*frameSize; 
+    public void addGrid(PointGeo BL, PointGeo TR, int resolution,
+            int howManyCells) {
         int decoratedMapWidth = map.getWidth();
-        int decoratedMapHeight = imageHeight + 2 * frameSize;
-        BufferedImage decoratedMap = new BufferedImage(decoratedMapWidth,
-                decoratedMapHeight, BufferedImage.TYPE_INT_RGB);
+        int decoratedMapHeight = map.getHeight();
+        int imageWidth = decoratedMapWidth - 2 * frameSize;
+        int imageHeight = decoratedMapHeight - 2 * frameSize;
 
-        Graphics2D canvas = decoratedMap.createGraphics();
-        canvas.setColor(Color.WHITE);
-        canvas.fillRect(0, 0, decoratedMapWidth, decoratedMapHeight);
-        canvas.drawImage(this.image(), frameSize, frameSize, this.image()
-                .getWidth(), this.image().getHeight(), null);
-        canvas.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
-        canvas.setStroke(new BasicStroke(0.05f * imageWidth / 100f));
+        graphicContext.setColor(Color.WHITE);
+        graphicContext.setStroke(new BasicStroke(0.05f * imageWidth / 100f));
 
         double radiansPerPixel = (TR.longitude() - BL.longitude())
                 / ((double) imageWidth);
 
         int squareSizePixel = (int) Math.round(imageWidth
-                / ((double) howManySquares));
+                / ((double) howManyCells));
 
         double squareSizeRadian = radiansPerPixel * squareSizePixel;
 
-        canvas.setFont(new Font("inconsolata", Font.PLAIN, 30));
+        graphicContext.setFont(new Font("inconsolata", Font.PLAIN, 30));
         int indexForStrings = 0;
         for (int x = frameSize + squareSizePixel; x < decoratedMapWidth
                 - frameSize; x += squareSizePixel) {
-            canvas.setColor(Color.GRAY);
-            canvas.draw(new Line2D.Double(x, frameSize, x, decoratedMapHeight
-                    - frameSize));
-            canvas.setColor(Color.BLACK);
+            graphicContext.setColor(Color.GRAY);
+            graphicContext.draw(new Line2D.Double(x, frameSize, x,
+                    decoratedMapHeight - frameSize));
+            graphicContext.setColor(Color.BLACK);
             double longitude = Math.toDegrees(BL.longitude() + indexForStrings
                     * squareSizeRadian);
             double minute = (longitude % 1) * 60;
             double second = (minute % 1) * 60;
-            canvas.drawString("" + (int) longitude + "°" + (int) minute + "'"
-                    + (int) second + "\"", x, frameSize);
-            canvas.setColor(Color.GRAY);
+            graphicContext.drawString("" + (int) longitude + "°" + (int) minute
+                    + "'" + (int) second + "\"", x, frameSize);
+            graphicContext.setColor(Color.GRAY);
             indexForStrings++;
         }
 
         indexForStrings = 0;
         for (int y = frameSize + squareSizePixel; y < decoratedMapHeight
                 - frameSize; y += squareSizePixel) {
-            canvas.setColor(Color.GRAY);
-            canvas.draw(new Line2D.Double(frameSize, y, decoratedMapHeight
-                    - frameSize, y));
-            canvas.setColor(Color.BLACK);
+            graphicContext.setColor(Color.GRAY);
+            graphicContext.draw(new Line2D.Double(frameSize, y,
+                    decoratedMapHeight - frameSize, y));
+            graphicContext.setColor(Color.BLACK);
             double latitude = Math.toDegrees(BL.latitude() + indexForStrings
                     * squareSizeRadian);
             double minute = (latitude % 1) * 60;
             double second = (minute % 1) * 60;
-            canvas.drawString("" + (int) latitude + "°" + (int) minute + "'"
-                    + (int) second + "\"", 0, y);
-            canvas.setColor(Color.GRAY);
+            graphicContext.drawString("" + (int) latitude + "°" + (int) minute
+                    + "'" + (int) second + "\"", 0, y);
+            graphicContext.setColor(Color.GRAY);
             indexForStrings++;
         }
-        setImage(decoratedMap);
+    }
+
+    public void printOnFile(String extension, String path) throws IOException {
+        ImageIO.write(map, extension, new File(path));
     }
 }
