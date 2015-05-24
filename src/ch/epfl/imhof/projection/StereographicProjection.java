688b3736d3741da7f6a5e631e9a6fe7b92fb1ee7
Vincenzo <vincenzo.bazzucchi@epfl.ch>
2015-05-24 11:51:50 +0200

    ajouté StereoGraphic Projection, la plus adaptée à des petites zones mais surtout aux zones polaires. Petit doutes sur le rayon à utiliser: j'ai utilisé le rayon de la terre é l'équateur mais la formule dit d'utiliser le "rayon local": 			Rayon à l'équateur * cos(lat) 			----------------------------- 			(1-e^2 sin^2(lat))*cos(Xi) rien de terrible tu dirais.... mais regarde le calcul de Xi: http://mathworld.wolfram.com/ConformalLatitude.html

---
+++ src/ch/epfl/imhof/projection/StereographicProjection.java
@@ -0,0 +1,52 @@
+package ch.epfl.imhof.projection;
+
+import ch.epfl.imhof.PointGeo;
+import ch.epfl.imhof.dem.Earth;
+import ch.epfl.imhof.geometry.Point;
+import static java.lang.Math.sin;
+import static java.lang.Math.cos;
+import static java.lang.Math.atan;
+import static java.lang.Math.asin;
+
+public class StereographicProjection implements Projection {
+    private final double centralLatitude;
+    private final double centralLongitude;
+
+    public StereographicProjection(double centralLongitude,
+            double centralLatitude) {
+        this.centralLatitude = Math.toRadians(centralLatitude);
+        this.centralLongitude = Math.toRadians(centralLongitude);
+    }
+
+    public StereographicProjection() {
+        this(0, 0);
+    }
+
+    @Override
+    public Point project(PointGeo point) {
+        double k = (2 * Earth.RADIUS)
+                / (1 + sin(centralLatitude) * sin(point.latitude()) + cos(centralLatitude)
+                        * cos(point.latitude())
+                        * cos(point.longitude() - centralLongitude));
+        double x = k * cos(point.latitude())
+                * sin(point.longitude() - centralLongitude);
+        double y = k
+                * (cos(centralLatitude) * sin(point.latitude()) - sin(centralLatitude)
+                        * cos(point.latitude())
+                        * cos(point.longitude() - centralLongitude));
+        return new Point(x, y);
+    }
+
+    @Override
+    public PointGeo inverse(Point point) {
+        double rho = Math.sqrt(Math.pow(point.x(), 2) + Math.pow(point.y(), 2));
+        double c = 2 * atan(rho / (2 * Earth.RADIUS));
+        double longitude = centralLongitude
+                + atan((point.x() * sin(c))
+                        / (rho * cos(centralLatitude) * cos(c) - point.y()
+                                * sin(centralLatitude) * sin(c)));
+        double latitude = asin(cos(c) * sin(centralLatitude)
+                + (point.y() * sin(c) * cos(centralLatitude)) / rho);
+        return new PointGeo(longitude, latitude);
+    }
+}
